b069a086986def2497e06f962722c1c7bb666cf5
xlh <xlh58404@163.com>
2019-05-15 19:16:39 +0800

    修改helloworld

--- src/com/itheima/test/HelloWorld.java
+++ src/com/itheima/test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.itheima.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello World!");
+        System.out.println("hi xiaohua");
     }
 }
