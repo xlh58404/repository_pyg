a8375ca6fa14d685a4f9456ebc53035bce9ed4b2
xlh <xlh58404@163.com>
2019-05-15 19:20:44 +0800

    第二次修改helloworld

--- src/com/itheima/test/HelloWorld.java
+++ src/com/itheima/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello World!");
         System.out.println("hi xiaohua");
+        System.out.println("hi xiaojunjun");
     }
 }
